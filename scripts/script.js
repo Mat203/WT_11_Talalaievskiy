a596d7cefbade1bdba674317e13a6bdf654ff013
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-10-16 15:39:55 +0300

    Moment js

--- scripts/script.js
+++ scripts/script.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     function displayData() {
         var content = '';
         _.forEach(data, function(item) {
-            content += '<p>Name: ' + item.name + ', Age: ' + dayjs().diff(item.birthdate, 'year') + '</p>';
+            content += '<p>Name: ' + item.name + ', Age: ' + dayjs().diff(item.birthdate, 'year') + ', Birthdate: ' + item.birthdate+'</p>';
         });
         $('#content').html(content);
     }
@@ -30,7 +30,10 @@ $(document).ready(function() {
 
     $('#button3').click(function() {
         _.forEach(data, function(item) {
-            item.birthdate = dayjs(item.birthdate).add(1, 'year').format('YYYY-MM-DD');
+            var newBirthdate = moment(item.birthdate).add(1, 'year');
+            if (moment().diff(newBirthdate, 'years') >= 0) {
+                item.birthdate = newBirthdate.format('YYYY-MM-DD');
+            }
         });
         displayData();
     });
@@ -46,6 +49,51 @@ $(document).ready(function() {
         _.forEach(data, function(item) {
             item.birthdate = dayjs().format('YYYY-MM-DD');
         });
+        displayData();
+    });
+
+    $('#button6').click(function() {
+        _.forEach(data, function(item) {
+            var newBirthdate = moment(item.birthdate).add(1, 'months');
+            if (moment().diff(newBirthdate, 'years') >= 0) {
+                item.birthdate = newBirthdate.format('YYYY-MM-DD');
+            }
+        });
+        displayData();
+    });
+    
+
+    $('#button7').click(function() {
+        _.forEach(data, function(item) {
+            item.birthdate = moment(item.birthdate).subtract(1, 'months').format('YYYY-MM-DD');
+        });
+        displayData();
+    });
+
+    $('#button8').click(function() {
+        _.forEach(data, function(item) {
+            item.birthdate = moment(item.birthdate).startOf('year').format('YYYY-MM-DD');
+        });
+        displayData();
+    });
+
+    $('#button9').click(function() {
+        _.forEach(data, function(item) {
+            item.birthdate = moment(item.birthdate).endOf('year').format('YYYY-MM-DD');
+        });
+        displayData();
+    });
+
+    $('#button10').click(function() {
+        _.forEach(data, function(item) {
+            item.birthdate = moment(item.birthdate, 'YYYY-MM-DD').add(1, 'days').format('YYYY-MM-DD');
+        });
+        displayData();
+    });    
+
+    $('#button11').click(function() {
+        var randomIndex = _.random(0, data.length - 1);
+        data.splice(randomIndex, 1);
         displayData();
     });
 
